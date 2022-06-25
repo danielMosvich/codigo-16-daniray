53fb28573a9c3bd065c408f1cd1332edc0570ad3
danirayGOD <mrdaniraymay@gmail.com>
2022-06-24 19:06:52 -0500

    termine los bucles de tarea

---
+++ semana4/js/bucles.js
@@ -0,0 +1,17 @@
+// let i = 0;
+// while (i < 10){
+// console.log(i)
+// i += 1;
+// }
+// console.log(i)
+
+// const alumnos = ['uwu','uwu2','uwu3','uwu4','uwu5']
+
+// let i = 0;
+// while (i < alumnos.length){
+//     console.log(alumnos[i])
+//     i += 1;
+// }
+
+let num1 = ()=> 2 + 2;
+console.log(num1());
