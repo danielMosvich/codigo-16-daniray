5ef9174ba7797a82d22d5aaaaf3106d8627db26a
danirayGOD <mrdaniraymay@gmail.com>
2022-07-13 23:14:49 -0500

    termino al clase de bootstrap 2

--- semana7/taskList/index.js
+++ semana7/taskList/index.js
@@ -1,175 +1,183 @@
-// document.querySelector
-// jquery
-
-/* $('element')
-Ejemplo
- h1
- $('h1')
- div
- $('div')
- id="container"
- $('#container')
- class="input-nombre"
- $('.input-nombre')
-*/
-
-//* Nota: Esta funcion que empieza con $(function) se
-//* ejecuta cuando la pagina web inicia
-//* Esto es solo en JQUERY
-// $(function () {
-//     // el acceder de esta forma a los elementos nos da acceso a las funciones de jquery
-//     $("h1").css("color", "red");
-//     // multiples estilos
-//     $("h1").css({
-//       "background-color": "blue",
-//       "font-size": "100px",
-//       "font-family": "Helvetica",
-//     });
-//   });
-  
-  //* En JS puro IIFE
-
-  // (function () {
-      //   // hacemos algo
-      // })();
-
-//
-//!variables
 const inputTask = $("#input-task");
 const btnTask = $("#btn-task");
+const formTask = $("#form-task");
+const form = document.querySelector("#form-task");
 const sectionTask = $("#section-task");
-//!esto se ejecuta al cargar la pagina
-// function deleteLoader(){
-//     arrayTask.forEach(task => {
-        
-//     })
-// }
+const sectionDetailTask = $("#section-detail-task");
+
 $(function () {
+  // vamos a obtener el queryString de la url
+  const queryString = new URLSearchParams(window.location.search);
+  const filter = queryString.get("filter"); // todo || done || delete
 
-    if(arrayTask.length > 0){
-        arrayTask.forEach(task => {
-            createInputTask(task.id, task.text,task.status)
-            if(task.status === 'delete'){
-            sectionTask.children('#'+task.id).children('button').hide()
-            }
-        });
+
+  if (arrayTask.length > 0) {
+    arrayTask.forEach((task) => {
+      createInputTask(task.id, task.text, task.status);
+    });
+
+    if (filter) {
+      showFilteredModelTask()
     }
-})
-//!al dar click s ejecuta la funcion
-btnTask.click(function () {
-    const text = inputTask.val();
-    const task = storeTask(text);
-    inputTask.val('')
-    inputTask.focus()
-    createInputTask(task.id,task.text,task.status)
-})
-//!funcion de borrar task
-function deletetask(element){
-    const div_task = $(element).closest('.row')
-    const id = div_task.data('id')
-    div_task.find('label').addClass('text-uppercase text-decoration-line-through')
+  }
+});
+function showFilteredModelTask(filter){
+    const filterTask = arrayTask.filter((task) => task.status === filter);
 
-    // const container = $(element).parent()
-    // container.children('span').css({
-    //     "text-decoration":"line-through",
-    // })
-    // container.children('button').hide()
-    updateTask(id,'status','delete')
-    console.log(div_task)
-    // container.children('span').style.color = 'red'
+      const tbody = $("#tbody");
 
-}
-//!edit task
-function editTask(element){
-    const div_task = $(element).closest('.row')
-    const id = div_task.data('id')
-    div_task.html(`
-        <div class='col-6 col-sm-8 col-md-9'>
+      filterTask.forEach((task) => {
+        tbody.append(
+          `<tr>
+            <td>${task.id}</td>
+            <td>${task.text}</td>
+            <td>${task.status}</td>
+            <td>${task.created_at}</td>
+          </tr>
+          `
+        );
+      });
 
-            <input class="form-control" type="text" placeholder="editar tarea..">
+      const myModal = new bootstrap.Modal(
+        document.getElementById("exampleModal")
+      );
 
-        </div>
-
-        <div class='col-6 col-sm-4 col-md-3' >
-            <button class="btn btn-dark" onclick="saveTask(this)">✅</button>
-        </div>
-    `)
-
-    // const container = $(element).parent()
-    // // container.focus()
-    // const id = container.children("input").data("id")
-    // container.children().hide()
-    // container.append(`
-    // <div class="container">
-    // <input type="text" placeholder="editar tarea.."> <button onclick="saveTask(this,${id})">✅</button>
-    // </div>
-    // `)
-    // //? focus al input creado
-    // container.children().children("input").focus()
+      myModal.show();
 }
 
-//!funtion save task
-function saveTask(element){
-    const div_task = $(element).closest('.row')
-    const id = Number(div_task.attr('data-id'))
-    const newText = div_task.find('input').val()
-    updateTask(id,'text',newText)
-    const task = arrayTask.find(task => task.id === id)
-    div_task.html(`
+formTask.submit(function (e) {
+  e.preventDefault();
+  inputTask.focus();
+  //! checkValidity analiza los inputs y si tienen required y estan vacios
+  //! esta funcion retorna false caso contrario esten lleno
+  //* retorna true
+  if (!form.checkValidity()) {
+    // agregamos la clase was-validated al formulario
+    // esto al ser agregado buscara los invalid-feedback y los mostrara
+    form.classList.add("was-validated");
+    return;
+  }
+
+  const text = inputTask.val();
+  const task = storeTask(text);
+  // console.log("Task", task);
+
+  inputTask.val("");
+
+  createInputTask(task.id, task.text, task.status);
+});
+
+function deleteTask(element) {
+  const div_task = $(element).closest(".row");
+  const id = div_task.data("id");
+  div_task
+    .find("label")
+    .addClass("text-uppercase text-decoration-line-through");
+  updateTask(id, "status", "delete");
+}
+
+function saveTask(element) {
+  const div_task = $(element).closest(".row");
+  const id = Number(div_task.attr("data-id"));
+  const newText = div_task.find("input").val();
+  updateTask(id, "text", newText);
+  const task = arrayTask.find((task) => task.id === id);
+  div_task.html(`
     <div class='col-6 col-sm-8 col-md-9'>
-
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="check_${id}">
-                 <label class="form-check-label ${task.status}" for="check_${id}">
-                 ${task.text}
-                </label>
-            </div>
-
-        </div>
-
-        <div class='col-6 col-sm-4 col-md-3' >
-            <button class="btn btn-light" onclick="editTask(this)">✏️</button>
-            <button class="btn btn-light">👁</button>
-            <button class="btn btn-dark" onclick="deletetask(this)">❌</button>
-        </div>
-    `)
-
+      <div class="form-check">
+        <input onchange="doneTask(this,${id})" class="form-check-input" type="checkbox" id="check_${id}">
+        <label class="form-check-label ${task.status}" for="check_${id}">
+          ${task.text}
+        </label>
+      </div>
+    </div>
+    <div class='col-6 col-sm-4 col-md-3'>
+      <button class='btn btn-light' onclick="editTask(this)" >✏️</button>
+      <button class='btn btn-light'>👁</button>
+      <button class='btn btn-dark' onclick="deleteTask(this)">❌</button>
+    </div>
+  `);
 }
-//!funcion de crear inputs
-function createInputTask(id,text,status){
-    $("<div class='row' data-id='"+id+"'>")
-        // id,
-        .html(
-        `
-        <div class='col-6 col-sm-8 col-md-9'>
 
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="check_${id}">
-                 <label class="form-check-label ${status}" for="check_${id}">
-                 ${text}
-                </label>
-            </div>
+function editTask(element) {
+  const div_task = $(element).closest(".row");
+//   const id = div_task.attr("data-id");
+  div_task.html(`
+      <div class='col-6 col-sm-8 col-md-9'>
+        <input placeholder="editar tarea" type="text" class="form-control"/>
+      </div>
+      <div class='col-6 col-sm-4 col-md-3'>
+        <button class="btn btn-dark" onclick="saveTask(this)">✅</button>
+      </div>
+  `);
+}
 
+function showTask(id) {
+  const task = arrayTask.find((task) => task.id === id);
+  sectionDetailTask.empty().show();
+
+  sectionDetailTask.append(
+    `
+    <div class="card" style="width: 300px; padding: 0;">
+      <img src="https://source.unsplash.com/user/erondu/300x200" class="card-img-top" />
+      <div class="card-body">
+        <div class="card-title">${task.text}</div>
+        <div class="card-text">
+        ${task.status}
         </div>
-
-        <div class='col-6 col-sm-4 col-md-3' >
-            <button class="btn btn-light" onclick="editTask(this)">✏️</button>
-            <button class="btn btn-light">👁</button>
-            <button class="btn btn-dark" onclick="deletetask(this)">❌</button>
+         <div class="card-text">
+        ${task.created_at}
         </div>
-        `)
+      </div>
+    </div>
+    `
+  )
+//   .fadeOut(5000);
+}
+function doneTask(element,id){
+    updateTask(id, "status", "done");
+    const div_task = $(element).closest(".row");
+    div_task.addClass('bg-success bg-opacity-50 rounded text-white fst-italic');
+    //! sus -_ -
+    location.reload()
     
+}
+function createInputTask(id, text, status) {
+    $(
+        `<div class='row py-2 my-2 ${
+          status === "done"
+            ? "bg-success bg-opacity-50 rounded text-white fst-italic"
+            : ""
+        }'  data-id='${id}'>`
+      )
+        .html(
+          `
+          <div class='col-6 col-sm-8 col-md-9'>
+            <div class="form-check">
+              <input ${
+                status === "done" ? "disabled checked" : ""
+              } class="form-check-input" type="checkbox" onchange="doneTask(this, ${id})" id="check_${id}">
+              <label class="form-check-label ${status}" for="check_${id}">
+                ${text}
+              </label>
+            </div>
+          </div>
+          <div class='col-6 col-sm-4 col-md-3'>
+            <button class='btn btn-light' onclick="editTask(this)" ${
+                status === "done"
+                  ? "style='display:none'"
+                  : ""
+              }>✏️</button>
+            <button class='btn btn-light' onclick="showTask(${id})"  >👁</button>
+            <button class='btn btn-dark' onclick="deleteTask(this)" ${
+                status === "done"
+                  ? "style='display:none'"
+                  : ""
+              }>❌</button>
+          </div>
+        `
+    )
     .appendTo(sectionTask)
-    // .hide()
-    // .fadeIn(1000)
-    // .class('row')
-
-    // $("div").hover(
-    //     function(){
-    //     $(this).css("background-color", "#ddd")
-    // },
-    // function(){
-    //     $(this).css("background-color", "transparent")
-    // }
-    // )
+    .hide()
+    .fadeIn(1000);
 }
